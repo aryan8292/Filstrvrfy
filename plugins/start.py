2e027f4c46f5ec4cd35373790054b2042d50451f
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-22 22:52:14 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -35,7 +35,7 @@ def store_verification_data(user_id, token, expiration_time):
     
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    user_id = message.from_user.id
+    user_id = message.from_user.id if message.from_user else None
     token = await get_verification_token(user_id)
     if not await present_user(id):
         try:
