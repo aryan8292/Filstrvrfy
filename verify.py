7ca8a540fba49db2bc54566b09973bd81a71c4e6
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-22 21:31:06 +0530

    Update verify.py

--- verify.py
+++ verify.py
@@ -45,28 +45,35 @@ async def get_token(bot, userid, link):
 
 async def verify_user(bot, userid, token):
     user = await bot.get_users(userid)
-    TOKENS[user.id] = {token: True}
+    
+    # Calculate the expiration time (24 hours from the current time)
     tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    VERIFIED[user.id] = str(today)
-
-    # Update the verification timestamp to mark the user as verified for 24 hours
-    expiration_time = datetime.now() + timedelta(hours=24)
+    expiration_time = datetime.now(tz) + timedelta(hours=24)
+    
+    # Store the expiration time in the VERIFIED dictionary
     VERIFIED[user.id] = expiration_time
 
+    # Return the Telegram bot URL
     return await generate_telegram_bot_url(bot_username)
 
 async def check_verification(bot, userid):
     user = await bot.get_users(userid)
     current_time = datetime.now()
+
     if user.id in VERIFIED.keys():
         expiration_time = VERIFIED[user.id]
+
         if current_time < expiration_time:
-            return True
+            return True  # User is verified
         else:
-            return False
+            return False  # Verification has expired
     else:
-        return False
+        return False  # User is not verified
 
 async def generate_telegram_bot_url(username):
     return f'tg://resolve?domain={username}&start=verified'
+
+if __name__ == "__main__":
+    # Replace 'bot_username' with your actual bot's username
+    bot_username = "@FileXTera_bot"
+    print("Bot URL:", generate_telegram_bot_url(bot_username))
