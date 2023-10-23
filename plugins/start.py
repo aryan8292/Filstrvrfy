e03a02c9ffd384fb20a4b07bce9079c3cf27e04c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-24 01:53:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -58,7 +58,8 @@ async def get_verification_token(user_id):
     verification_token = secrets.token_hex(16)  # Generates a 32-character hexadecimal token
 
     # Calculate the expiration time (24 hours from now)
-    expiration_time = datetime.now() + timedelta(hours=24)
+        current_time = datetime.now()
+        expiration_time = current_time + timedelta(hours=24) 
 
     # Define the status of the token as "active"
     status_of_token = "active"
