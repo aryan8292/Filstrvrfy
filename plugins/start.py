23e1a3dcab860ef92d84d37a2e7f980e89b4579b
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-24 19:49:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -14,7 +14,7 @@ from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
-from verify import TOKENS, VERIFIED, check_token, get_token, verify_user, check_verification
+from verify import TOKENS, VERIFIED, check_token, get_token, verify_user, check_verification, get_shortlink
 
 SECONDS = int(os.getenv("SECONDS", "10")) #add time im seconds for waitingwaiting before delete
 
