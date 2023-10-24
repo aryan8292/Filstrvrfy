51d66bca88cab758858c723fb2cd92335b5790fa
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-10-24 17:11:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -80,6 +80,9 @@ async def get_verification_token(user_id):
 
         return verification_token
 
+async def get_token(client, user_id):
+    token = await get_verification_token(user_id)  # Call get_verification_token to retrieve the token
+    return token
 
 async def is_verified_user(user_id):
     # Connect to the MongoDB database
@@ -158,7 +161,7 @@ async def start_command(client, message):
             # Create a button for verification
             button = InlineKeyboardButton(
                 "Verify",
-                url=await get_verification_token(client, user_id, f"https://telegram.me/{client.username}?start=verify-{user_id}-{token}")
+                url=await get_token(client, user_id, f"https://telegram.me/{client.username}?start=verify-{user_id}-{token}")
             )
 
             # Create a reply markup with the verification button
